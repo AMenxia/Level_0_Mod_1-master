fbb9916fb3697b660e8ef3c297f676f69c26a7be
kmksmfvkmsfkmvkfsk <rishikhurana2@gmail.com>
2017-09-12 17:47:57 -0700

    Xgeeks

--- Level_0_Mod_1-master/src/variables_and_conditionals/XGeeks.java
+++ Level_0_Mod_1-master/src/variables_and_conditionals/XGeeks.java
@@ -11,10 +11,29 @@ public class XGeeks {
 public static void main(String[] args) {
 
         // 1. Save the superpower for each person in a variable.
-
+String June = "Flying";
+String Zed = "Shadow Clones";
+String Kayne = "Walk walking";
+String Ivern = "Being a Tree";
         // 2. Ask the user to enter a name. Store their answer in a variable.
-
+String Charater = JOptionPane.showInputDialog("You have just met these characters from leage of ledgens in real life,Zed, June, Kayne, and Ivern. You can find out their super powers IRL by typing their name in this message box");
         // 3. Show the superpower in a pop-up, depending on the name entered. 
+if (Charater.equalsIgnoreCase("zed")){
+	JOptionPane.showInputDialog(Zed);
+
+}
+else if (Charater.equalsIgnoreCase("kayne")){
+	JOptionPane.showInputDialog(Kayne);
+
+}
+else if (Charater.equalsIgnoreCase("June")){
+	JOptionPane.showInputDialog(June);
+
+}
+else if (Charater.equalsIgnoreCase("Ivern")){
+	JOptionPane.showInputDialog(Ivern);
+
+}
 
     }
 }
